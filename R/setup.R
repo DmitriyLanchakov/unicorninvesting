f0c53a5206d7f760dd6299a74cc5472695b299ca
achillesrasquinha <achillesrasquinha@gmail.com>
2017-07-17 13:05:15 +0530

    updated setup.R

--- R/setup.R
+++ R/setup.R
@@ -7,6 +7,7 @@ log.DEBUG   <<- TRUE
 log.info('setup', paste('Installing necessary dependencies:', join(REQUIRED_PACKAGES, ', ')))
 install.package(REQUIRED_PACKAGES, mirror = mirror, dependencies = TRUE)
 
+# Shit, why? What was I even thinking?
 if ( dir.exists(path.CACHE) ) {
   unlink(path.CACHE, recursive = TRUE, force = TRUE)
 }
